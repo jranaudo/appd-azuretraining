77eb3ccf002b7bd7a5fe5926c75a5d00c2ecb2df
jranaudo <jranaudo@gmail.com>
2020-01-30 18:12:43 +0000

    Initial Commit

---
+++ DotNetAppSqlDb/Controllers/TodosController.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DotNetAppSqlDb.Models;
+using System.Diagnostics;
+using System.Text;
+using System.Net.Http;
+using System.IO;
+
+namespace DotNetAppSqlDb.Controllers
+{
+    public class TodosController : Controller
+    {
+        private MyDatabaseContext db = new MyDatabaseContext();
+
+        // GET: Todos
+        public ActionResult Index()
+        {            
+            Trace.WriteLine("GET /Todos/Index");
+            return View(db.Todoes.ToList());
+        }
+
+        // GET: Todos/Details/5
+        public ActionResult Details(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Details/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Create
+        public ActionResult Create()
+        {
+            Trace.WriteLine("GET /Todos/Create");
+            return View(new Todo { CreatedDate = DateTime.Now });
+        }
+
+        // POST: Todos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Description,CreatedDate")] Todo todo)
+        {
+            Trace.WriteLine("POST /Todos/Create");
+            if (ModelState.IsValid)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://jptestf001.azurewebsites.net/api/Function1?code=H14uuB7PASwcn1jQLH7hkXeuBa4ZkG24worJWKsu0OaMWYKnF9QnAQ==&name=JohnRanaudo");
+                request.Method = "GET";
+
+                try
+                {
+                    WebResponse webResponse = request.GetResponse();
+                    //Stream webStream = webResponse.GetResponseStream();
+                    //StreamReader responseReader = new StreamReader(webStream);
+                    //string response = responseReader.ReadToEnd();
+                    //Console.Out.WriteLine(response);
+                    //responseReader.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine("-----------------");
+                    Console.Out.WriteLine(e.Message);
+                }
+
+
+                db.Todoes.Add(todo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        // GET: Todos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Edit/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Description,CreatedDate")] Todo todo)
+        {
+            Trace.WriteLine("POST /Todos/Edit/" + todo.ID);
+            if (ModelState.IsValid)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            Trace.WriteLine("GET /Todos/Delete/" + id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trace.WriteLine("POST /Todos/Delete/" + id);
+            Todo todo = db.Todoes.Find(id);
+            db.Todoes.Remove(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FunctionApp1/Function1.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dummy.restapiexample.com/api/v1/employees");
+            request.Method = "GET";
+
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                Stream webStream = webResponse.GetResponseStream();
+                StreamReader responseReader = new StreamReader(webStream);
+                string response = responseReader.ReadToEnd();
+                Console.Out.WriteLine(response);
+                responseReader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("-----------------");
+                Console.Out.WriteLine(e.Message);
+            }
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
